b2361bf2d1087429dcf8dbd59b2cee67bb622baf
liyuan-hu <lyscu@foxmail.com>
2024-08-15 14:59:41 +0800

    引入easyui

---
+++ gmall-manage-web/src/main/java/com/atguigu/gmall/manage/Controller/ManageController.java
@@ -0,0 +1,12 @@
+package com.atguigu.gmall.manage.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ManageController {
+    @RequestMapping(value = "index")
+    public String index(){
+        return "index";
+    }
+}
